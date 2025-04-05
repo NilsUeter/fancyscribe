4424356dbc47e248d74a8c48ca15ad040e7ef77b
NilsUeter <nils.ueter@gmail.com>
2025-04-05 19:08:30 +0200

    add react-compiler

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react({
+			babel: {
+				plugins: [["babel-plugin-react-compiler"]],
+			},
+		}),
+	],
 	base: "/fancyscribe",
 });
